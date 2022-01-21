9603ce80ecdbdf5050dd599041fd30f41da4da71
VARUN KASHYAP.K.S <varunkashyapks@gmail.com>
2022-01-21 22:52:03 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,1 +1,17 @@
-
+class Stack:
+  
+  def __init__(self):
+    self.data = []
+  
+  def push(self,value):
+    self.data.append(value)
+    
+  def pop(self):
+    if len(self.data) <= 0:
+      print('No elements to popup')
+    else:
+      self.data.pop()
+   
+  def peek(self):
+    return self.data[0]
+  
